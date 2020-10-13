e9570ded4761cbf93709482f111912423a6f45e2
Jacob Pierce <jacobpierce@physics.ucla.edu>
2020-10-12 21:22:32 -0700

    glob3

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup
 
 setup(	name = 'osiris_suite',
 		install_requires = [ 'colorcet', 'matplotlib', 'numpy', 
-					'glob', 'pathos', 'h5py' ] ) 
+					'glob3', 'pathos', 'h5py' ] ) 
